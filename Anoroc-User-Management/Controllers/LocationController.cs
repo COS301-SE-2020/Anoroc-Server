a2135a6bda8ed325204982d38bc6556fe607e05d
Andrew Wilson <u15191223@tuks.co.za>
2020-06-08 16:49:33 +0200

    Added Location endpoint skeleton

---
+++ Anoroc-User-Management/Controllers/LocationController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Anoroc_User_Management.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class LocationController : ControllerBase
+    {
+        [HttpPost]
+        public string Post()
+        {
+            return "this is a post";
+        }
+    }
+}
