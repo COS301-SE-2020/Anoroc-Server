ab15372924b61792dfbf5b8410b6d539ba53e70c
Tebza <tn.selahle@gmail.com>
2020-06-12 09:17:23 +0200

    Added Login unit test

---
+++ UserManagementTest/LoginTest.cs
@@ -0,0 +1,19 @@
+using NUnit.Framework;
+
+namespace UserManagementTest
+{
+    //TODO: Add test cases
+    public class Login
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
