c7110199fbd559b1288425cf532fd938c589343c
Tebza <tn.selahle@gmail.com>
2020-06-13 11:41:27 +0200

    Added tests

--- UserManagementTest/LoginTest.cs
+++ UserManagementTest/LoginTest.cs
@@ -1,19 +1,53 @@
+using System.Threading.Tasks;
+using Anoroc_User_Management.Controllers;
+using Anoroc_User_Management.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
 using NUnit.Framework;
 
 namespace UserManagementTest
 {
     //TODO: Add test cases
-    public class Login
+    public class LoginTest
     {
+        private LoginController _loginController; 
         [SetUp]
         public void Setup()
         {
+            var request = new Mock<HttpRequest>();
+            request.Setup(x => x.Scheme).Returns("http");
+            request.Setup(x => x.Host).Returns(HostString.FromUriComponent("http://localhost:5001"));
+            request.Setup(x => x.PathBase).Returns(PathString.FromUriComponent("/userManagement"));
+            
+            var httpContext = Mock.Of<HttpContext>(_ => 
+                _.Request == request.Object
+            );
+
+            //Controller needs a controller context 
+            var controllerContext = new ControllerContext() {
+                HttpContext = httpContext,
+            };
+
+            _loginController = new LoginController(){
+                ControllerContext = controllerContext,
+            };
         }
 
         [Test]
         public void Test1()
         {
-            Assert.Pass();
+            const string expected = "expected value here";
+            var test = new Login()
+            {
+                Email = "tn.selahle@gmail.com",
+                Password = "thisIs"
+            };
+
+            var actual =  _loginController.Post(test);
+            
+            Assert.AreEqual(expected, actual);
+            
         }
     }
 }
