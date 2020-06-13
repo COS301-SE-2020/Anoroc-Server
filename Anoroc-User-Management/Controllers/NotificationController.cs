9afd25acd2a47aeb1d0e9577c2441dd59a73f408
Kevin-D-H <u15026681@tuks.co.za>
2020-06-13 23:41:54 +0200

    Added Basic Notification Route

---
+++ Anoroc-User-Management/Controllers/NotificationController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using Anoroc_User_Management.Models;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace Anoroc_User_Management.Controllers
+{
+    [ApiController]
+    public class NotificationContoller : ControllerBase
+    {
+        [HttpGet("notification/all")] 
+        public ActionResult<IEnumerable<Notification>> GetAll()
+        {
+            return new []
+            {
+                new Notification { description = "This is a mock Notification" },
+            };
+        }
+    }
+    
+ 
+}
