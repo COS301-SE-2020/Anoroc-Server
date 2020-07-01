8947c6908777db12882b400a0dac1b9aef6edf0d
Andrew Wilson <u15191223@tuks.co.za>
2020-07-01 16:23:00 +0200

    Fixing Architecture

--- Anoroc-User-Management/Services/CrossedPathService.cs
+++ Anoroc-User-Management/Services/CrossedPathService.cs
@@ -8,7 +8,7 @@ namespace Anoroc_User_Management.Services
     /// <summary>
     /// Service called everytime the client sends a new location point.
     /// This service calculates if a user has come in contact with a carrier, it then sends the location points to the ClusterService to see if the user is getting close
-    /// to a hotspot area
+    /// to a hotspot area.
     /// </summary>
     public class CrossedPathService
     {
