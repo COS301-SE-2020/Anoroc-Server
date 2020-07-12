1d976cb7eaaca765aafc664eff78413df6ca2b6f
Tebogo <tn.selahle@gmail.com>
2020-07-12 17:55:14 +0200

    Added CrossedPathsService

---
+++ Anoroc-User-Management/Services/CrossedPathsService.cs
@@ -0,0 +1,10 @@
+﻿namespace Anoroc_User_Management.Services
+{
+    /// <summary>
+    /// Class responsible for providing functionality to check if users have crossed paths or not.
+    /// </summary>
+    public class CrossedPathsService
+    {
+        
+    }
+}
