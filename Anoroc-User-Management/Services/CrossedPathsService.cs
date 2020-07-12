9dd8bdfb551742cf4cfb3b77794b8da5aa422ecb
Tebogo <tn.selahle@gmail.com>
2020-07-12 19:20:30 +0200

    Added ProcessLocation function

--- Anoroc-User-Management/Services/CrossedPathsService.cs
+++ Anoroc-User-Management/Services/CrossedPathsService.cs
@@ -1,10 +1,20 @@
-﻿namespace Anoroc_User_Management.Services
+﻿using Anoroc_User_Management.Models;
+
+namespace Anoroc_User_Management.Services
 {
     /// <summary>
     /// Class responsible for providing functionality to check if users have crossed paths or not.
     /// </summary>
     public class CrossedPathsService
     {
-        
+        /// <summary>
+        /// Processes a location point.
+        /// If the point falls within a cluster (in danger), the user has to be notified.
+        /// </summary>
+        /// <param name="location">A location point that has to be processed</param>
+        public void ProcessLocation(Location location)
+        {
+            //TODO Consider making this async
+        }
     }
 }
