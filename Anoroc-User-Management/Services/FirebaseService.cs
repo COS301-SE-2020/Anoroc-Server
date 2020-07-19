3fcee7a930e720510edf8d089120ab6ee89295c9
Tebogo <tn.selahle@gmail.com>
2020-07-20 00:50:23 +0200

    Added data to be sent with the notification

--- Anoroc-User-Management/Services/FirebaseService.cs
+++ Anoroc-User-Management/Services/FirebaseService.cs
@@ -1,12 +1,15 @@
 ﻿using Anoroc_User_Management.Interfaces;
 using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Threading.Tasks;
+using Anoroc_User_Management.Models;
 
 // Firebase
 using FirebaseAdmin;
 using FirebaseAdmin.Messaging;
 using Google.Apis.Auth.OAuth2;
+using Notification = FirebaseAdmin.Messaging.Notification;
 
 
 namespace Anoroc_User_Management.Services
@@ -22,6 +25,9 @@ namespace Anoroc_User_Management.Services
             _messaging = FirebaseMessaging.GetMessaging(_defaultApp);
         }
 
+        /// <summary>
+        /// Configures the Firebase app to be used
+        /// </summary>
         private void Config()
         {
             _defaultApp = FirebaseApp.Create(new AppOptions()
@@ -32,22 +38,37 @@ namespace Anoroc_User_Management.Services
 
         }
 
-        public async Task SendNotification()
+        /// <summary>
+        /// Sends a notification to firebase asynchronously
+        /// </summary>
+        /// <returns>A Task object</returns>
+        public async Task SendNotification(Location location)
         {
-            var result = await _messaging.SendAsync(CreateNotification());
+            var result = await _messaging.SendAsync(CreateNotification(location));
             Console.WriteLine(result);
         }
 
-        private Message CreateNotification()
+        /// <summary>
+        /// Creates a notification to be sent to firebase
+        /// </summary>
+        /// <returns>A Message object</returns>
+        private Message CreateNotification(Location location)
         {
             return new Message()
             {
+                // get firebase token from database using location.token
                 Token =
                     "eKpj5XPkQP8:APA91bG1NWX8akOflJSpmQoduQzgYDMolTsjKQXr4QX5ZMXLvzcGrKdsazjRWahamLXBWjNVlrthHcJ-ybzd5YWKbU_tvzQroYHhMq_Oy-YVCzQ-HnJ7sT3_tuxVko4Qixo5hml9CkyP",
                 Notification = new Notification()
                 {
-                    Body = "You have come into contact",
-                    Title = "Contagaion Encounter"
+                    Body = "You have come into contact with a carrier. Click for more info",
+                    Title = "Contagion Encounter"
+                },
+                Data = new Dictionary<string, string>()
+                {
+                    { "DateTime", location.Created.ToString() },
+                    { "longitude", location.Coordinate.Longitude.ToString() },
+                    { "Latitude", location.Coordinate.Latitude.ToString() }
                 }
             };
         }
