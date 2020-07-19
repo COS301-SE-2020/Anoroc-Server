6eaa61eab2f3669b9d2bb8f0a0177649af30b955
Tebogo <tn.selahle@gmail.com>
2020-07-19 21:26:31 +0200

    Added the NotificationServerService interface

---
+++ Anoroc-User-Management/Interfaces/INotificationServerService.cs
@@ -0,0 +1,18 @@
+﻿namespace Anoroc_User_Management.Interfaces
+{
+    /// <summary>
+    /// Interface for each notification server. i.e. Firebase, Azure Notification Hub
+    /// </summary>
+    public interface INotificationServerService
+    {
+        /// <summary>
+        /// Configure the server service
+        /// </summary>
+        public void Config();
+        
+        /// <summary>
+        /// Send a notification to the server
+        /// </summary>
+        public void SendNotification();
+    }
+}
