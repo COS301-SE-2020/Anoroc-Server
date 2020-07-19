f17b2b6a0cb2164f7d78a8eec6b5a79e15cf57a5
Anika <anikavanrensburg@gmail.com>
2020-07-19 17:06:53 +0200

    Carrier status endpoint added

---
+++ Anoroc-User-Management/Controllers/UserManagementController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Anoroc_User_Management.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class UserManagementController : ControllerBase
+    {
+        public UserManagementController()
+        {
+
+        }
+
+        [HttpPost("CarrierStatus")]
+        public string CarrierStatus([FromBody]string status) {
+            return status;
+        }
+    }
+}
