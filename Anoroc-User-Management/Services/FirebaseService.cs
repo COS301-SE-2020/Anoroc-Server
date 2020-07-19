d9a552507414be37f2d837ff4d528fac4db87060
Tebogo <tn.selahle@gmail.com>
2020-07-19 21:28:57 +0200

    Created Firebase service with config

---
+++ Anoroc-User-Management/Services/FirebaseService.cs
@@ -0,0 +1,37 @@
+﻿using Anoroc_User_Management.Interfaces;
+using System;
+using System.IO;
+
+// Firebase
+using FirebaseAdmin;
+using FirebaseAdmin.Messaging;
+using Google.Apis.Auth.OAuth2;
+
+
+namespace Anoroc_User_Management.Services
+{
+    public class FirebaseService : INotificationService
+    {
+        private FirebaseApp DefaultApp;
+        public FirebaseService()
+        {
+            Config();
+        }
+
+        private void Config()
+        {
+            DefaultApp = FirebaseApp.Create(new AppOptions()
+            {
+                Credential = GoogleCredential.FromFile(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "key.json")),
+            });
+            
+            // TODO: Remove this line
+            Console.WriteLine(DefaultApp.Name);
+        }
+
+        public void SendNotification()
+        {
+            
+        }
+    }
+}
