22e21d8d127d3428e6d4abba24b6617df73efffe
Tebogo <tn.selahle@gmail.com>
2020-07-19 23:24:58 +0200

    Implemented mock data notification delivery

--- Anoroc-User-Management/Services/FirebaseService.cs
+++ Anoroc-User-Management/Services/FirebaseService.cs
@@ -1,6 +1,7 @@
 ﻿using Anoroc_User_Management.Interfaces;
 using System;
 using System.IO;
+using System.Threading.Tasks;
 
 // Firebase
 using FirebaseAdmin;
@@ -12,26 +13,43 @@ namespace Anoroc_User_Management.Services
 {
     public class FirebaseService : IMobileMessagingClient
     {
-        private FirebaseApp DefaultApp;
+        private FirebaseApp _defaultApp;
+        private readonly FirebaseMessaging _messaging;
+
         public FirebaseService()
         {
             Config();
+            _messaging = FirebaseMessaging.GetMessaging(_defaultApp);
         }
 
         private void Config()
         {
-            DefaultApp = FirebaseApp.Create(new AppOptions()
+            _defaultApp = FirebaseApp.Create(new AppOptions()
             {
-                Credential = GoogleCredential.FromFile(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "key.json")),
+                Credential =
+                    GoogleCredential.FromFile(Path.Combine(AppDomain.CurrentDomain.BaseDirectory!, "key.json")),
             });
-            
-            // TODO: Remove this line
-            Console.WriteLine(DefaultApp.Name);
+
         }
 
-        public void SendNotification()
+        public async Task SendNotification()
         {
-            
+            var result = await _messaging.SendAsync(CreateNotification());
+            Console.WriteLine(result);
+        }
+
+        private Message CreateNotification()
+        {
+            return new Message()
+            {
+                Token =
+                    "eKpj5XPkQP8:APA91bG1NWX8akOflJSpmQoduQzgYDMolTsjKQXr4QX5ZMXLvzcGrKdsazjRWahamLXBWjNVlrthHcJ-ybzd5YWKbU_tvzQroYHhMq_Oy-YVCzQ-HnJ7sT3_tuxVko4Qixo5hml9CkyP",
+                Notification = new Notification()
+                {
+                    Body = "You have come into contact",
+                    Title = "Contagaion Encounter"
+                }
+            };
         }
     }
 }
