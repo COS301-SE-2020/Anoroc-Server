e2457db8b1e08e00231397fa23e8c96dd8e8d080
Andrew Wilson <u15191223@tuks.co.za>
2020-07-20 10:56:26 +0200

    Usermanagement endpoint for firebase token

--- Anoroc-User-Management/Controllers/UserManagementController.cs
+++ Anoroc-User-Management/Controllers/UserManagementController.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using Anoroc_User_Management.Interfaces;
 using Anoroc_User_Management.Models;
 using Microsoft.AspNetCore.Mvc;
 
@@ -8,13 +9,24 @@ namespace Anoroc_User_Management.Controllers
     [ApiController]
     public class UserManagementController : ControllerBase
     {
-        public UserManagementController()
+        IDatabaseEngine DatabaseService;
+        public UserManagementController(IDatabaseEngine database)
         {
-
+            DatabaseService = database;
         }
 
         [HttpPost("CarrierStatus")]
         public string CarrierStatus([FromBody] Token token_object) {
+            var returnString = token_object.Object_To_Server + "";
+            return returnString;
+        }
+
+        [HttpPost("FirebaseToken")]
+        public string FirebaseToken([FromBody] Token token_object)
+        {
+
+            DatabaseService.InsertFirebaseToken(token_object.access_token, (token_object.Object_To_Server+""));
+
             var returnString = token_object.Object_To_Server + "";
             return returnString;
         }
