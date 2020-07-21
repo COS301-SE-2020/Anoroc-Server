06b85d73d2ce519a4af6d76f9ca4517565797f85
Anrich96 <anrich96@gmail.com>
2020-07-21 11:46:00 +0200

    Query to add a user to database
    Do it, run the query

---
+++ SQL Queries/Attempt to Insert into Location.sql
@@ -0,0 +1,4 @@
+USE Anoroc
+
+INSERT INTO dbo.Locations(Coordinate,Carrier_Data_Point,Created,Region)
+values('{"Lat":37.4219983333333,"lat":-122.084}',1,'20200721 10:19:00 AM','{"Country":"Merica","Provice":"California","Suburb":"Vegas"}')
