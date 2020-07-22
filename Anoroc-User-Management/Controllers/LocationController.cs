9d399ddd2b280c304ce18e4d71fe2a7c97e2a46f
Tebogo <30689767+TNSelahle@users.noreply.github.com>
2020-07-22 11:49:55 +0200

    Update LocationController.cs

--- Anoroc-User-Management/Controllers/LocationController.cs
+++ Anoroc-User-Management/Controllers/LocationController.cs
@@ -13,7 +13,6 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore.Storage;
 using Nancy.Json;
 using System.Text.Json;
-using Newtonsoft.Json;
 
 namespace Anoroc_User_Management.Controllers
 {
