9c6906ae91e3c769be1be0cee289370cc586bc8a
Anika <anikavanrensburg@gmail.com>
2020-08-04 10:48:05 +0200

    Changed return value of Itinerary endpoint in ItineraryController

---
+++ Anoroc-User-Management/Controllers/ItineraryController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Threading.Tasks;
+using Anoroc_User_Management.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Anoroc_User_Management.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class ItineraryController : ControllerBase
+    {
+        public ItineraryController()
+        {
+        }
+
+        [HttpPost("Itinerary")]
+        public IActionResult Itinerary([FromBody] Token token_object)
+        {            
+            return Ok(token_object.Object_To_Server);
+        }
+    }
+}
