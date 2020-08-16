373d89962ffcf7c2fde95db86051b88cedf61a9e
Andrew Wilson <u15191223@tuks.co.za>
2020-08-16 17:57:55 +0200

    Added token generating algorithm

--- Anoroc-User-Management/Services/TokenGenerator.cs
+++ Anoroc-User-Management/Services/TokenGenerator.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
 using System.Threading.Tasks;
 
 namespace Anoroc_User_Management.Services
@@ -12,7 +14,20 @@ namespace Anoroc_User_Management.Services
     {
         public static string NewToken()
         {
-            throw new NotImplementedException();
+            int size = 128;
+            var charSet = "abcdefghijkmnopqrstuvwxyzABCDEFGHJKLMNPQRSTUVWXYZ23456789";
+            var chars = charSet.ToCharArray();
+            var data = new byte[1];
+            var crypto = new RNGCryptoServiceProvider();
+            crypto.GetNonZeroBytes(data);
+            data = new byte[size];
+            crypto.GetNonZeroBytes(data);
+            var result = new StringBuilder(size);
+            foreach (var b in data)
+            {
+                result.Append(chars[b % (chars.Length)]);
+            }
+            return result.ToString();
         }
     }
 }
