f56b79e50baf68bd52c27459bc05b2cb1d7baddc
Tebogo <tn.selahle@gmail.com>
2020-08-19 08:22:25 +0200

    Initialised integration test

---
+++ Anoroc-User-Management.Testing/CustomWebApplicationFactory.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Linq;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+namespace Anoroc_User_Management.Testing
+{
+    public class CustomWebApplicationFactory<TStartup>
+        : WebApplicationFactory<TStartup> where TStartup: class
+    {
+        
+    }
+}
---
+++ Anoroc-User-Management.Testing/UnitTest1.cs
@@ -0,0 +1,13 @@
+using System;
+using Xunit;
+
+namespace Anoroc_User_Management.Testing
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+        }
+    }
+}
