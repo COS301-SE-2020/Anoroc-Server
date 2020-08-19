68dfb3af955049fe007e43c9f55e2bdb8c0efdb5
Tebogo <tn.selahle@gmail.com>
2020-08-19 10:24:37 +0200

    Set up in memory database

--- Anoroc-User-Management.Testing/CustomWebApplicationFactory.cs
+++ Anoroc-User-Management.Testing/CustomWebApplicationFactory.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Linq;
+using Anoroc_User_Management.Models;
+using Anoroc_User_Management.Testing.Helpers;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Testing;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
@@ -10,6 +12,40 @@ namespace Anoroc_User_Management.Testing
     public class CustomWebApplicationFactory<TStartup>
         : WebApplicationFactory<TStartup> where TStartup: class
     {
-        
+        protected override void ConfigureWebHost(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices(services =>
+            {
+                var descriptor = services.SingleOrDefault(
+                    d => d.ServiceType ==
+                         typeof(DbContextOptions<AnorocDbContext>));
+
+                services.Remove(descriptor);
+
+                services.AddDbContext<AnorocDbContext>(options =>
+                {
+                    options.UseInMemoryDatabase("InMemoryDbForTesting");
+                });
+
+                var sp = services.BuildServiceProvider();
+
+                using (var scope = sp.CreateScope())
+                {
+                    var scopeServices = scope.ServiceProvider;
+                    var db = scopeServices.GetRequiredService<AnorocDbContext>();
+
+                    db.Database.EnsureCreated();
+
+                    try
+                    {
+                        Utilities.InitializeDbForTests(db);
+                    }
+                    catch (Exception e)
+                    {
+                        Console.WriteLine(e);
+                    }
+                }
+            });
+        }
     }
 }
---
+++ Anoroc-User-Management.Testing/Helpers/Utilities.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using Anoroc_User_Management.Models;
+
+namespace Anoroc_User_Management.Testing.Helpers
+{
+    public static class Utilities
+    {
+        public static void InitializeDbForTests(AnorocDbContext db)
+        {
+            db.Users.AddRange(GetSeedingUsers());
+            db.SaveChanges();
+        }
+
+        public static void ReinitializeDbForTests(AnorocDbContext db)
+        {
+            db.Users.RemoveRange(db.Users);
+            InitializeDbForTests(db);
+        }
+
+        public static List<User> GetSeedingUsers()
+        {
+            return new List<User>()
+            {
+                new User()
+                {
+                    AccessToken = "12345abcd",
+                    carrierStatus = false,
+                    currentlyLoggedIn = true,
+                    Email = "tn.selahle@gmail.com",
+                    Firebase_Token = Guid.NewGuid().ToString(),
+                    FirstName = "Tebogo",
+                    UserSurname = "Selahle",
+                    Location = new Location(55.5, 12.2),
+                    loggedInAnoroc = false,
+                    loggedInFacebook = false,
+                    loggedInGoogle = false
+                },
+                new User()
+                {
+                    AccessToken = Guid.NewGuid().ToString(),
+                    carrierStatus = false,
+                    currentlyLoggedIn = true,
+                    Email = "anrich96@gmail.com",
+                    Firebase_Token = Guid.NewGuid().ToString(),
+                    FirstName = "Anrich",
+                    UserSurname = "Hildebrand",
+                    Location = new Location(155.5, 122.2),
+                    loggedInAnoroc = false,
+                    loggedInFacebook = false,
+                    loggedInGoogle = false
+                },
+                new User()
+                {
+                    AccessToken = Guid.NewGuid().ToString(),
+                    carrierStatus = true,
+                    currentlyLoggedIn = true,
+                    Email = "jacob.zuma@gmail.com",
+                    Firebase_Token = Guid.NewGuid().ToString(),
+                    FirstName = "Jacob",
+                    UserSurname = "Zuma",
+                    Location = new Location(195.3, 132.2),
+                    loggedInAnoroc = false,
+                    loggedInFacebook = false,
+                    loggedInGoogle = false,
+                    
+                }
+            };
+        }
+    }
+}
