11e18c0ed9f8317b595fd33f2d444c24bac5a461
Anrich96 <anrich96@gmail.com>
2020-08-20 02:05:03 +0200

    Updated Insert Location query

--- SQL Queries/Attempt to Insert into Location.sql
+++ SQL Queries/Attempt to Insert into Location.sql
@@ -1,4 +1,7 @@
 USE Anoroc
 
-INSERT INTO dbo.Locations(Coordinate,Carrier_Data_Point,Created,Region)
-values('{"Latitude":37.4219983333333,"Longitude":-122.084,"HorizontalAccuracy":"NaN","VerticalAccuracy":"NaN","Speed":"NaN","Course":"NaN","IsUnknown":false,"Altitude":"NaN"}',1,'20200721 10:19:00 AM','{"Country":"Merica","Provice":"California","Suburb":"Vegas"}')
+INSERT INTO dbo.Areas (Country,Province,Suburb)
+VALUES ('America','California','Berkeley')
+
+INSERT INTO dbo.Locations(Latitude,Longitude,Carrier_Data_Point,Created,RegionArea_ID)
+values(37.4219983333333,-122.084,1,'20200721 10:19:00 AM',1)
