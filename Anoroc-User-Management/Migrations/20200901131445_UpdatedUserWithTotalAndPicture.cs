fd8aa4cef182d2f5d7f129d6467ca1e9c67180bb
Anrich96 <anrich96@gmail.com>
2020-09-01 15:16:32 +0200

    Added Total Incidents and Profile Picture to User

---
+++ Anoroc-User-Management/Migrations/20200901131445_UpdatedUserWithTotalAndPicture.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Anoroc_User_Management.Migrations
+{
+    public partial class UpdatedUserWithTotalAndPicture : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<byte[]>(
+                name: "ProfilePicture",
+                table: "Users",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "totalIncidents",
+                table: "Users",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ProfilePicture",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "totalIncidents",
+                table: "Users");
+        }
+    }
+}
