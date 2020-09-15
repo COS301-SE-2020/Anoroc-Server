3ee710a0c8d2b1920c53eb54aa626e1d0da873d2
Kevin <kevinhuang4058@gmail.com>
2020-09-15 16:16:44 +0200

    Added test function when starting up

--- Anoroc-User-Management/Startup.cs
+++ Anoroc-User-Management/Startup.cs
@@ -27,6 +27,11 @@ namespace Anoroc_User_Management
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+
+            PredictionService ps = new PredictionService();
+            ps.predicateSuburbConfirmed();
+
+
             services.AddAuthentication(AzureADB2CDefaults.BearerAuthenticationScheme)
                 .AddAzureADB2CBearer(options => Configuration.Bind("AzureAdB2C", options));
             services.AddControllers();
